e9e521f91b4427097d733005a830a752a0dadac4
Gaurav Tekude <Gauravtekude1998@gmail.com>
2026-08-03 17:06:19 +0530

    updated index.js cors

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-examnotes-generatorclient.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
